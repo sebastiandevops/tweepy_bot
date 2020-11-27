ea52dd4634c7f5ff51758de11064c8be985ba421
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-11-27 11:07:01 -0500

    update

--- hello_tweepy_public.py
+++ hello_tweepy_public.py
@@ -16,13 +16,17 @@ def tweet_job():
     except:
         print("Error during authentication")
     # Create a tweet
-    # open Robin's Edgar Allen Poe data file and read every line into memory
+    # open data file and read every line into memory
     with open('/path/to/file.txt','r') as filename:
         lines = filename.readlines()
+    #select random line from lines
     myline =random.choice(lines)
 
-    # Tweet each line, then wait one minute and tweet another.
-    # Note: this design means the bot runs continuously
+    # Tweet my line
+    # Yoy have to add this script to crontab -e to automatically runs.
+    # For example, if you add this line to crontab
+    # 0 9 * * * /full/path/to/script/hello_tweepy.py
+    # The script will execute every day at 9am.
     myline = myline + '#thingsthathappened'
     mystr = myline.replace("\n"," ")
     api.update_status(status=mystr)
