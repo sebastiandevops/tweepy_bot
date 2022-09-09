6399c4b4f1d389743f561f5703954b0afc8d4529
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-09-09 08:11:58 -0500

    History update: 09/09/22 08:11:56

--- tweet.sh
+++ tweet.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+timestamp=$(date +"%D %T")
 cd /home/sebastian/estudio/tweepy_bot
 git pull origin main
 python3 my_bot
 git add .
-git commit -m "History update"
+git commit -m "History update: $timestamp"
 git push origin main
