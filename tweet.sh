318bb17d7b5707cd215181130cec73802c5b605d
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-22 14:41:03 -0500

    History update: 05/22/23 14:41:01

--- tweet.sh
+++ tweet.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 timestamp=$(date +"%D %T")
 cd /home/sebastian/estudio/tweepy_bot || exit
-# git pull origin main
-./tweepy_botV2.0.sh
+git pull origin main
 python3 my_bot
-rm -rf processed* data.txt today_in_history.txt
-# git add .
-# git commit -m "History update: $timestamp"
-# git push origin main
+git add .
+git commit -m "History update: $timestamp"
+git push origin main
