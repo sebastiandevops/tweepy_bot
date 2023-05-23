a5c298630e47f0eebd593de2ae9a07085f6faa73
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-23 05:55:00 -0500

    tweepy_botV2 updated to handle more corner cases when scraping data from web page

--- tweepy_botV2.sh
+++ tweepy_botV2.sh
@@ -4,6 +4,6 @@ echo $(curl --silent https://www.historynet.com/today-in-history/ | htmlq div | 
 sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' data.txt > processed.txt
 grep -E '[0-9]{4}\s' processed.txt > processed2.txt
 cat processed2.txt | egrep -o '^[^.]+' > processed3.txt
-grep -v -e '\[' -e '\]' -e 'Lottery' -e 'See more' processed3.txt | grep -vE '^.{,50}$' > processed4.txt
+grep -v -e '\[' -e '\]' -e '(' -e 'Lottery' -e 'See more' processed3.txt | grep -vE '^.{,50}$' > processed4.txt
 sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
 sed -i 's/$/./' today_in_history.txt
--- tweet_v2.sh
+++ tweet_v2.sh
@@ -2,7 +2,7 @@
 timestamp=$(date +"%D %T")
 cd /home/sebastian/estudio/tweepy_bot || exit
 # git pull origin main
-./tweepy_botV2.0.sh
+./tweepy_botV2.sh
 python3 my_bot_v2
 rm -rf processed* data.txt today_in_history.txt
 # git add .
