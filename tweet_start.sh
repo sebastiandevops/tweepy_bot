51175f7b85f8a4a0e9ab06b821a990f1c5633030
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-24 18:51:05 -0500

    History update: 05/24/23 18:51:01

--- tweet_start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-timestamp=$(date +"%D %T")
-cd /home/sebastian/estudio/tweepy_bot || exit
-git pull origin main
-./tweepy_botV2.sh
-python3 my_bot_start
-git add .
-git commit -m "History update: $timestamp"
-git push origin main
---
+++ tweet_v2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+timestamp=$(date +"%D %T")
+file_path="$HOME/estudio/tweepy_bot/today_in_history.txt"
+cd /home/sebastian/estudio/tweepy_bot || exit
+git pull origin main
+if [ -f "$file_path" ]; then
+    echo "File exists. Executing my_bot_end and data cleaner."
+    # Place your commands to be executed if the file exists here
+    python3 my_bot_end
+    rm -rf processed* data.txt today_in_history.txt
+else
+    echo "File does not exist. Executing tweepy_botV2 to scrape data and my_bot_start"
+    # Place your commands to be executed if the file does not exist here
+    ./tweepy_botV2.sh
+    python3 my_bot_start
+fi
+git add .
+git commit -m "History update: $timestamp"
+git push origin main
