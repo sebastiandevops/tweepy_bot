966d017fc712bc2bb54d91ab5b1f09cb8c4eb3ae
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-25 07:19:05 -0500

    tweepy_botV2 britannica updated to handle corner cases. Scraped data added to repo

--- tweepy_botV2_britannica.sh
+++ tweepy_botV2_britannica.sh
@@ -4,6 +4,6 @@ echo $(curl --silent https://www.britannica.com/on-this-day | htmlq --text | htm
 sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' data.txt > processed.txt
 grep -E '[0-9]{4}\s' processed.txt > processed2.txt
 cat processed2.txt | egrep -o '^[^.]+' > processed3.txt
-grep -v -e 'Lottery' processed3.txt | grep -vE '^.{,50}$' > processed4.txt
+grep -v -e 'See All' processed3.txt | grep -vE '^.{,50}$' > processed4.txt
 sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
 sed -i 's/$/./' today_in_history.txt
