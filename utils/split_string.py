0dac512ae866c9dd3310355782fab694e452f9db
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-25 15:12:02 -0500

    tweepy modules updated to enable threads History update: 05/25/23 15:11:59

--- utils/split_string.py
+++ utils/split_string.py
@@ -2,8 +2,15 @@
 
 def split_string(string):
     if len(string) > 240:
-        first_string = string[:240]  # First 240 characters
-        second_string = string[240:]  # Remaining characters
+        # First 240 characters
+        first_string = string[:240]
+        # Find the last space within the first 240 characters
+        last_space_index = first_string.rfind(' ')
+        if last_space_index != -1:
+            # Truncate to the last space
+            first_string = first_string[:last_space_index]
+        # Remaining characters
+        second_string = string[len(first_string):].strip()
     else:
         first_string = string
         second_string = ""
