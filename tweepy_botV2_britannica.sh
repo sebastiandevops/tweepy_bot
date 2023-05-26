92085b8b62345b973c74618f00531e817ad24586
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-26 07:25:34 -0500

    tweepy_bot scraper updated to handle dots preceded by uppercase or followed by commans

--- tweepy_botV2_britannica.sh
+++ tweepy_botV2_britannica.sh
@@ -2,8 +2,7 @@
 # Secuence
 echo $(curl --silent https://www.britannica.com/on-this-day | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > data.txt
 sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' data.txt > processed.txt
-grep -E '[0-9]{4}\s' processed.txt > processed2.txt
-cat processed2.txt | egrep -o '^[^.]+' > processed3.txt
-grep -v -e 'See All' processed3.txt | grep -vE '^.{,50}$' > processed4.txt
+sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' processed.txt > processed2.txt
+grep -E '[0-9]{4}\s' processed2.txt > processed3.txt
+grep -v -e 'See All' processed3.txt | grep -vE '^.{,60}$' > processed4.txt
 sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
-sed -i 's/$/./' today_in_history.txt
