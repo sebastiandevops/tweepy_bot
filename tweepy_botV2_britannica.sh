d90465afc5e24a45ff85748c6393e2b62cbdae3a
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-26 07:40:39 -0500

    tweepy_bot scraper updated to avoid comma insert afert a year not followed by a space

--- tweepy_botV2_britannica.sh
+++ tweepy_botV2_britannica.sh
@@ -4,5 +4,6 @@ echo $(curl --silent https://www.britannica.com/on-this-day | htmlq --text | htm
 sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' data.txt > processed.txt
 sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' processed.txt > processed2.txt
 grep -E '[0-9]{4}\s' processed2.txt > processed3.txt
-grep -v -e 'See All' processed3.txt | grep -vE '^.{,60}$' > processed4.txt
-sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
+grep -v -e 'See All' -e 'SHOW' processed3.txt | grep -vE '^.{,60}$' > processed4.txt
+sed -E 's/([0-9]{4}) /\1, /g' processed4.txt > today_in_history.txt
+# sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
