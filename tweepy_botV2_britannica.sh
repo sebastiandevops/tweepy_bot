ccece1c045ed40e95864a762cd963d3d09288379
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-27 06:00:05 -0500

    History update: 05/27/23 06:00:01

--- tweepy_botV2_britannica.sh
+++ tweepy_botV2_britannica.sh
@@ -3,9 +3,8 @@
 on_this_day_url="https://www.onthisday.com/"
 britannica_url="https://www.britannica.com/on-this-day"
 echo $(curl --silent $britannica_url | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > data.txt
-sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' data.txt > processed.txt
-sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' processed.txt > processed2.txt
+sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' data.txt > processed.txt
+sed -E 's/([0-9]{4}\s)/\n\1/' processed.txt > processed2.txt
 grep -E '[0-9]{4}\s' processed2.txt > processed3.txt
-grep -v -e 'See All' -e 'SHOW' processed3.txt | grep -vE '^.{,60}$' > processed4.txt
+grep -v -e 'See All' -e 'SHOW' -e 'Test your knowledge' processed3.txt | grep -vE '^.{,120}$' > processed4.txt
 sed -E 's/([0-9]{4}) /\1, /g' processed4.txt > today_in_history.txt
-# sed -E 's/([0-9]{4})([^s])/\1,\2/g' processed4.txt > today_in_history.txt
