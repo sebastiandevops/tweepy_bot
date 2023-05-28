4dc8f3805f7d5f0ca002fb007d872b9ecc6c8bd5
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-28 06:00:04 -0500

    History update: 05/28/23 06:00:01

--- tweepy_botV2_spanish.sh
+++ tweepy_botV2_spanish.sh
@@ -4,12 +4,11 @@ hoy_en_la_historia="https://www.hoyenlahistoria.com/efemerides.php"
 ap_news="https://apnews.com/hub/today-in-history"
 on_this_day_url="https://www.onthisday.com/"
 britannica_url="https://www.britannica.com/on-this-day"
-rm -rf hoy_en_la_historia.txt
 echo $(curl --silent $hoy_en_la_historia | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > datos.txt
 sed -r 's/(.)([0-9]{4}\s)/\1\n\2/g' datos.txt > output.txt
 sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' output.txt > output2.txt
 grep -E '[0-9]{4}\s' output2.txt > output3.txt
-grep -v -e 'See All' -e 'SHOW' -e 'Efemérides' output3.txt | grep -vE '^.{,80}$' > output4.txt
+grep -v -e 'See All' -e 'SHOW' -e 'Efemérides' output3.txt | grep -vE '^.{,120}$' > output4.txt
 sed -E 's/([0-9]{4}) /\1, /g' output4.txt > output5.txt
 sed 's/, - /, /g' output5.txt > hoy_en_la_historia.txt
 rm -rf output* datos*
---
+++ tweepy_modules/modularized_bot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import tweepy
+
+import random
+from utils.split_string import split_string
+# Authenticate to Twitter
+
+
+def tweet_job():
+    auth = tweepy.OAuthHandler("API_KEY", "API_SECRET")
+    auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+    # Create API object
+    api = tweepy.API(auth)
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except SystemExit:
+        print("Error during authentication")
+        raise
+    # Create a tweet
+    # open Robin's Edgar Allen Poe data file and read every line into memory
+    with open('/home/sebastian/estudio/tweepy_bot/history.txt', 'r') as filename:
+        lines = filename.readlines()
+
+    myline = random.choice(lines)
+
+    lines.pop(lines.index(myline))
+
+    with open('/home/sebastian/estudio/tweepy_bot/history.txt', 'w') as filename:
+        filename.writelines(lines)
+
+    # Tweet each line, then wait one minute and tweet another.
+    # Note: this design means the bot runs continuously
+    myline = myline
+    mystr = myline.strip()
+    mystr = "🤖 #History " + mystr
+
+    if len(mystr) <= 240:
+        original_tweet = api.update_status(status=mystr)
+        print(mystr)
+    else:
+        firstStr, secondStr = split_string(mystr)
+        firstStr = firstStr + " [1/2]"
+        secondStr = secondStr + " [2/2]"
+        original_tweet = api.update_status(status=firstStr)
+        reply1_tweet = api.update_status(status=secondStr,
+                                         in_reply_to_status_id=original_tweet.id,
+                                         auto_populate_reply_metadata=True)
+        print(f"First tweet: {firstStr}\nsecond tweet: {secondStr}")
+
+
+if __name__ == "__main__":
+    main()
