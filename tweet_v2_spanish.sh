660acbe59faf9ece2a83500d730f0db1b217c0de
Sebastián Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-28 20:27:27 -0500

    Update tweet_v2_spanish.sh

--- tweet_v2_spanish.sh
+++ tweet_v2_spanish.sh
@@ -3,7 +3,7 @@ url="https://www.hoyenlahistoria.com/efemerides.php"
 timestamp=$(date +"%D %T")
 cd /home/sebastian/estudio/tweepy_bot || exit
 git pull origin main
-./scrapers/hoy_en_la_historia_scraper.sh $url
+./scrapers/hoy_en_la_historia_scraper.sh "$url"
 python3 bot_spanish_runner.py
 git add .
 git commit -m "bot_spanish_runner executed: $timestamp"
