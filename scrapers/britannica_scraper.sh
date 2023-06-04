519ca8b9e34fd7cb243c2f12a6dd51828d22b521
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-06-04 06:02:42 -0500

    My_bot_start executed: 06/04/23 06:02:38

--- scrapers/britannica_scraper.sh
+++ scrapers/britannica_scraper.sh
@@ -6,6 +6,6 @@ echo $(curl --silent "$url" | htmlq --text | html2text) | tr -s ' ' | sed '/./G'
 sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' "$dir"/data.txt > "$dir"/processed.txt
 sed -E 's/([0-9]{4}\s)/\n\1/' "$dir"/processed.txt > "$dir"/processed2.txt
 grep -E '[0-9]{4}\s' "$dir"/processed2.txt > "$dir"/processed3.txt
-grep -v -e 'See All' -e 'SHOW' -e 'Test your knowledge' "$dir"/processed3.txt | grep -vE '^.{,120}$' > "$dir"/processed4.txt
+grep -v -e 'See All' -e 'SHOW' -e 'Test your knowledge' -e 'born' "$dir"/processed3.txt | grep -vE '^.{,120}$' > "$dir"/processed4.txt
 sed -E 's/([0-9]{4}) /\1, /g' "$dir"/processed4.txt > "$dir"/today_in_history.txt
 rm -rf "$dir"/data.txt "$dir"/processed*
