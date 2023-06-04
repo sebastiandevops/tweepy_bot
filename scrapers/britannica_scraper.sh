eba5b9a938b52a7c3c3704cc68d0a5db2ce88102
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-06-04 08:55:44 -0500

    britannica scraper updated to handle 'born, sort fact, how much and test your knowledge' corner cases

--- scrapers/britannica_scraper.sh
+++ scrapers/britannica_scraper.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Secuence
 url=$1
-dir=$HOME/estudio/tweepy_bot/scrapers
+dir="$HOME"/estudio/tweepy_bot/scrapers
 echo $(curl --silent "$url" | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > "$dir"/data.txt
+sed -i '/Sort fact from fiction/s//\n&/g' data.txt
+sed -i '/How much do you know about/s//\n&/g' data.txt
+sed -i '/Test your knowledge/s//\n&/g' data.txt
 sed -E 's/([^A-Z])\.([^,])/\1.\n\2/g' "$dir"/data.txt > "$dir"/processed.txt
 sed -E 's/([0-9]{4}\s)/\n\1/' "$dir"/processed.txt > "$dir"/processed2.txt
 grep -E '[0-9]{4}\s' "$dir"/processed2.txt > "$dir"/processed3.txt
-grep -v -e 'See All' -e 'SHOW' -e 'Test your knowledge' -e 'born' "$dir"/processed3.txt | grep -vE '^.{,120}$' > "$dir"/processed4.txt
+grep -v -e 'born' "$dir"/processed3.txt | grep -vE '^.{,80}$' > "$dir"/processed4.txt
 sed -E 's/([0-9]{4}) /\1, /g' "$dir"/processed4.txt > "$dir"/today_in_history.txt
 rm -rf "$dir"/data.txt "$dir"/processed*
