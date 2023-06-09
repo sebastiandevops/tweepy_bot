1f87cca2765ef29cfdf318d9d3b6e08de0b925e7
Sebastián Valencia <sebasvalencia726@hotmail.com>
2023-06-09 05:15:22 -0500

    Improved script to include facts occured before year 100

--- scrapers/hoy_en_la_historia_scraper.sh
+++ scrapers/hoy_en_la_historia_scraper.sh
@@ -5,7 +5,7 @@
 url=$1
 
 # Define the directory path
-dir="$HOME/estudio/tweepy_bot/scrapers"
+dir="$HOME/estudio/tweepy_bot/scrapers/"
 
 # Remove the existing hoy_en_la_historia.txt file
 rm -rf "$dir"/hoy_en_la_historia.txt
@@ -13,8 +13,8 @@ rm -rf "$dir"/hoy_en_la_historia.txt
 # Fetch the content from the given URL, convert it to plain text, remove extra spaces, and save it to data.txt
 echo $(curl --silent "$url" | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > "$dir"/data.txt
 
-# Insert a new line before a 3 or 4-digit number followed by a hyphen in data.txt
-sed -i -E 's/([0-9]{3,4} -)/\n\1/g' "$dir"/data.txt
+# Insert a new line before either a 2 to 4 digit number followed by a hyphen or a space, followed by a 2 to 4 digit number, followed by "a.C. -" in data.txt
+sed -i -E 's/([0-9]{2,4} -| [0-9]{2,4} a\.C\. -)/\n\1/g' "$dir"/data.txt
 
 # Remove empty lines from data.txt
 sed -i '/^\s*$/d' "$dir"/data.txt
@@ -27,6 +27,9 @@ grep -v -e 'See All' -e 'SHOW' -e 'Efemérides' "$dir"/data.txt | grep -vE '^.{,
 
 # Replace ' - ' with ', ' in hoy_en_la_historia.txt
 sed -i 's/ - /, /g' "$dir"/hoy_en_la_historia.txt
+
+# removes any leading white spaces at the beginning of each line in the file.
+sed -i 's/^[[:space:]]*//' "$dir"/hoy_en_la_historia.txt
 
 # Remove the temporary output* and datos* files
 rm -rf "$dir"/data.txt
