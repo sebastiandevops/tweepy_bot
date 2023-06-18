8ea8495f6919f6a42561ecac36bb51513978ea25
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-06-18 11:31:16 -0500

    api_v2

--- cronjobs/tweet_v2.sh
+++ cronjobs/tweet_v2.sh
@@ -3,22 +3,25 @@ timestamp=$(date +"%D %T")
 url="https://www.hoyenlahistoria.com/efemerides.php"
 dir="$HOME/estudio/tweepy_bot/"
 file_path="$dir/hoy_en_la_historia.txt"
+scraper="$dir"/scrapers/hoy_en_la_historia_scraper.sh
+branch="api_v2"
+
 cd "$dir" || exit
-git pull origin feature/api_v2
+git pull origin "$branch"
 if [ -f "$file_path" ]; then
     echo "hoy_en_la_historia exists. Executing my_bot_end and data cleaner."
     # Place your commands to be executed if the file exists here
     python3 "$dir"/bot_end_runner.py
-    rm -rf "$dir"/scrapers/hoy_en_la_historia.txt
+    rm -rf "$file_path"
     git add .
     git commit -m "My_bot_end executed: $timestamp"
-    git push origin feature/api_v2
+    git push origin "$branch"
 else
     echo "hoy_en_la_historia does not exist. Executing tweepy_botV2 to scrape data and my_bot_start"
     # Place your commands to be executed if the file does not exist here
-    bash "$dir"/scrapers/hoy_en_la_historia_scraper.sh "$url"
+    bash "$scraper" "$url"
     python3 "$dir"/bot_start_runner.py
     git add .
     git commit -m "My_bot_start executed: $timestamp"
-    git push origin feature/api_v2
+    git push origin "$branch"
 fi
--- cronjobs/tweet_v2_english.sh
+++ cronjobs/tweet_v2_english.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 url="https://www.britannica.com/on-this-day"
 timestamp=$(date +"%D %T")
-dir="$HOME/estudio/tweepy_bot/"
+dir="$HOME/estudio/tweepy_bot"
+scraper="$dir"/scraper/britannica_scraper.sh
+bot_runner="bot_english_runner.py"
+branch="api_v2"
+
 cd "$dir" || exit
-git pull origin feature/api_v2
-bash "$dir"/scrapers/britannica_scraper.sh "$url"
-python3 "$dir"/bot_english_runner.py
+git pull origin "$branch"
+bash "$scraper" "$url"
+python3 "$dir"/"$bot_runner"
 git add .
-git commit -m "bot_english_runner executed: $timestamp"
-git push origin feature/api_v2
+git commit -m "$bot_runner executed: $timestamp"
+git push origin "$branch"
