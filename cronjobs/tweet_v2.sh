ea8380e71df40805aeee076540615906444658f3
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-07-01 14:02:08 -0500

    bash script cronjobs updated

--- cronjobs/tweet_v2.sh
+++ cronjobs/tweet_v2.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
-timestamp=$(date +"%D %T")
+
+# url to scrape data
 url="https://www.hoyenlahistoria.com/efemerides.php"
+
+# paths
 dir="$HOME/estudio/tweepy_bot"
 file_path="$dir"/scrapers/hoy_en_la_historia.txt
 scraper="$dir"/scrapers/hoy_en_la_historia_scraper.sh
-branch="main"
 
-cd "$dir" || exit
-git pull origin "$branch"
+# Get timestamp
+timestamp=$(date +"%D %T")
+
+# Change directory and handle potential errors
+cd "$dir" || {
+    echo "Failed to change directory to $dir"
+    exit 1
+}
+
+# Pull changes from the current branch and check for errors
+git pull origin "$(git rev-parse --abbrev-ref HEAD)"
+pull_exit_code=$?
+if [[ $pull_exit_code -ne 0 ]]; then
+    echo "Failed to pull changes from Git. Exit code: $pull_exit_code"
+    exit 1
+fi
+
+# Check if file exists
 if [ -f "$file_path" ]; then
     echo "hoy_en_la_historia exists. Executing my_bot_end and data cleaner."
     # Place your commands to be executed if the file exists here
@@ -15,7 +33,7 @@ if [ -f "$file_path" ]; then
     rm -rf "$file_path"
     git add .
     git commit -m "My_bot_end executed: $timestamp"
-    git push origin "$branch"
+    git push origin "$(git rev-parse --abbrev-ref HEAD)"
 else
     echo "hoy_en_la_historia does not exist. Executing tweepy_botV2 to scrape data and my_bot_start"
     # Place your commands to be executed if the file does not exist here
@@ -23,5 +41,5 @@ else
     python3 "$dir"/bot_start_runner.py
     git add .
     git commit -m "My_bot_start executed: $timestamp"
-    git push origin "$branch"
+    git push origin "$(git rev-parse --abbrev-ref HEAD)"
 fi
--- cronjobs/tweet_v2_english.sh
+++ cronjobs/tweet_v2_english.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
+
+# url to scrape data
 url="https://www.britannica.com/on-this-day"
-timestamp=$(date +"%D %T")
+
+# paths
 dir="$HOME/estudio/tweepy_bot"
 scraper="$dir"/scrapers/britannica_scraper.sh
 bot_runner="bot_english_runner.py"
-branch="main"
 
-cd "$dir" || exit
-git pull origin "$branch"
+# Get timestamp
+timestamp=$(date +"%D %T")
+
+# Change directory and handle potential errors
+cd "$dir" || {
+    echo "Failed to change directory to $dir"
+    exit 1
+}
+
+# Pull changes from the current branch and check for errors
+git pull origin "$(git rev-parse --abbrev-ref HEAD)"
+pull_exit_code=$?
+if [[ $pull_exit_code -ne 0 ]]; then
+    echo "Failed to pull changes from Git. Exit code: $pull_exit_code"
+    exit 1
+fi
+
+# run scraper and bot
 bash "$scraper" "$url"
 python3 "$dir"/"$bot_runner"
+
+# remote update
 git add .
 git commit -m "$bot_runner executed: $timestamp"
-git push origin "$branch"
+git push origin "$(git rev-parse --abbrev-ref HEAD)"
