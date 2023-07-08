8f0bb8f9605a18ed0ec08c5d9ac7e91357376a5a
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-07-08 13:09:24 -0500

    hoy_en_la_history_scraper updated to handle new webpage feature: Acontecimiento

--- scrapers/hoy_en_la_historia_scraper.sh
+++ scrapers/hoy_en_la_historia_scraper.sh
@@ -4,7 +4,6 @@
 
 # Set the URL argument passed to the script
 url=$1
-
 # Define the directory path
 dir="$HOME"/estudio/tweepy_bot/scrapers
 
@@ -16,6 +15,9 @@ echo $(curl --silent "$url" | htmlq --text | html2text) | tr -s ' ' | sed '/./G'
 
 # Insert a new line after the "Efemérides por día" line in data.txt
 sed -i '/Efemérides por día/s//\n&/g' "$dir"/data.txt
+
+# insert a new line both before the word "Acontecimiento"
+sed -i '/Acontecimiento/s//\n&/g' "$dir"/data.txt
 
 # Insert a new line after the "Todos los tiposEfeméridesCumpleañosMuertes" line in data.txt
 sed -i 's/Todos los tiposEfeméridesCumpleañosMuertes/&\n/g' "$dir"/data.txt
@@ -43,6 +45,22 @@ sed -i 's/ *$//' "$dir"/hoy_en_la_historia.txt
 
 # removes any leading white spaces at the beginning of each line in the file.
 sed -i 's/^[[:space:]]*//' "$dir"/hoy_en_la_historia.txt
+
+# remove the content after the last dot (excluding the dot itself), but only if the line does not end with a parenthesis symbol.
+sed -i -E '/\)\s*$/!s/\.[^.]*$/\./' "$dir"/hoy_en_la_historia.txt
+
+#Handle Acontecimiento
+# Command 1: Insert a new line before the last sequence of 3 or 4 numbers in lines starting with "Acontecimiento"
+sed -i -E '/^Acontecimiento/ s/([0-9]{3,4})([^0-9]*$)/\n\1\2/' "$dir"/hoy_en_la_historia.txt
+
+# Command 2: Remove everything after the last dot in lines starting with "Acontecimiento"
+sed -i -E '/^Acontecimiento/ s/\.([^.]*)$/\./' "$dir"/hoy_en_la_historia.txt
+
+# Command 3: Remove the word "Acontecimiento" from lines starting with that word
+sed -i -E 's/^Acontecimiento //' "$dir"/hoy_en_la_historia.txt
+
+# Command 4: Add a comma after the last sequence of 2, 3, or 4 numbers in lines
+sed -i -E ':a; s/([0-9]{2,4}) /\1, /; t; n; ba' "$dir"/hoy_en_la_historia.txt
 
 # Remove the temporary output* and datos* files
 rm -rf "$dir"/data.txt
