4d2aed2b88b9bc526ee0eaac8ae615e1e2524813
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-07-08 06:23:42 -0500

    My_bot_start executed: 07/08/23 06:23:38

--- scrapers/hoy_en_la_historia_scraper.sh
+++ scrapers/hoy_en_la_historia_scraper.sh
@@ -13,8 +13,8 @@ rm -rf "$dir"/hoy_en_la_historia.txt
 # Fetch the content from the given URL, convert it to plain text, remove extra spaces, and save it to data.txt
 echo $(curl --silent "$url" | htmlq --text | html2text) | tr -s ' ' | sed '/./G' > "$dir"/data.txt
 
-# Insert a new line before either a 2 to 4 digit number followed by a hyphen or a space, followed by a 2 to 4 digit number, followed by "a.C. -" in data.txt
-sed -i -E 's/([0-9]{2,4} -| [0-9]{2,4} a\.C\. -)/\n\1/g' "$dir"/data.txt
+# Insert a new line before either a 2 to 4 digit number followed by a space or by a 2 to 4 digit number, followed by "a.C. " in data.txt
+sed -i -E 's/([0-9]{2,4} | [0-9]{2,4} a\.C\. )/\n\1/g' "$dir"/data.txt
 
 # Remove empty lines from data.txt
 sed -i '/^\s*$/d' "$dir"/data.txt
@@ -23,18 +23,20 @@ sed -i '/^\s*$/d' "$dir"/data.txt
 sed -i '$ s/\./.\n/' "$dir"/data.txt
 
 # Filter out lines containing 'See All', 'SHOW', 'Efemérides' from output.txt and remove lines shorter than or equal to 140 characters, save the result to hoy_en_la_historia.txt
-grep -v -e 'See All' -e 'SHOW' -e 'Efemérides' "$dir"/data.txt | grep -vE '^.{,140}$' > "$dir"/hoy_en_la_historia.txt
+grep -v -e 'See All' -e 'SHOW' -e 'Efemérides' "$dir"/data.txt | grep -vE '^.{,80}$' > "$dir"/hoy_en_la_historia.txt
 
 # Replace ' - ' with ', ' in hoy_en_la_historia.txt
-sed -i 's/ - /, /g' "$dir"/hoy_en_la_historia.txt
+# sed -i 's/ - /, /g' "$dir"/hoy_en_la_historia.txt
+sed -i -E 's/([0-9]{2,4}) /\1, /g' "$dir"/hoy_en_la_historia.txt
 
 # remove all possible spaces at the end of the line
-sed -i 's/[[:blank:]]*$//' "$dir"/hoy_en_la_historia.txt
+sed -i 's/ *$//' "$dir"/hoy_en_la_historia.txt
 
 # removes any leading white spaces at the beginning of each line in the file.
 sed -i 's/^[[:space:]]*//' "$dir"/hoy_en_la_historia.txt
 
 # remove the content after the last dot (excluding the dot itself), but only if the line does not end with a parenthesis symbol.
+# sed -i -E 's/\.[^.]*$/\./' "$dir"/hoy_en_la_historia.txt
 sed -i -E '/\)\s*$/!s/\.[^.]*$/\./' "$dir"/hoy_en_la_historia.txt
 
 # Remove the temporary output* and datos* files
