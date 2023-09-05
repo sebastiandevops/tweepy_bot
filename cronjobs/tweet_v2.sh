30ad9a99c5f9f763b6a356696fd3ef11ed9d8aab
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-09-05 17:12:52 -0500

    My_bot_end executed: 09/05/23 17:12:49

--- cronjobs/tweet_v2.sh
+++ cronjobs/tweet_v2.sh
@@ -26,25 +26,35 @@ if [[ $pull_exit_code -ne 0 ]]; then
 fi
 
 # Check if file exists
-if [ -f "$file_path" ]; then
-    echo "hoy_en_la_historia exists. Executing my_bot_end and data cleaner."
-    # Place your commands to be executed if the file exists here
-    python3 "$dir"/bot_end_runner.py
-    rm -rf "$file_path"
-    git add .
-    git commit -m "My_bot_end executed: $timestamp"
-    git push origin "$(git rev-parse --abbrev-ref HEAD)"
-else
-    echo "hoy_en_la_historia does not exist. Executing tweepy_botV2 to scrape data and my_bot_start"
-    # Place your commands to be executed if the file does not exist here
-    bash "$scraper" "$url"
-    python3 "$dir"/bot_start_runner.py
-    scraper_exit_code=$(wc -l hoy_en_la_historia.txt | tr -dc '0-9')
-    if [[ $scraper_exit_code -ne 0 ]]; then
-        echo "Failed to scrape data from url: $scraper_exit_code"
-        exit 1
-    fi
-    git add .
-    git commit -m "My_bot_start executed: $timestamp"
-    git push origin "$(git rev-parse --abbrev-ref HEAD)"
-fi
+# if [ -f "$file_path" ]; then
+#     echo "hoy_en_la_historia exists. Executing my_bot_end and data cleaner."
+#     # Place your commands to be executed if the file exists here
+#     python3 "$dir"/bot_end_runner.py
+#     rm -rf "$file_path"
+#     git add .
+#     git commit -m "My_bot_end executed: $timestamp"
+#     git push origin "$(git rev-parse --abbrev-ref HEAD)"
+# else
+#     echo "hoy_en_la_historia does not exist. Executing tweepy_botV2 to scrape data and my_bot_start"
+#     # Place your commands to be executed if the file does not exist here
+#     bash "$scraper" "$url"
+#     python3 "$dir"/bot_start_runner.py
+#     scraper_exit_code=$(wc -l hoy_en_la_historia.txt | tr -dc '0-9')
+#     if [[ $scraper_exit_code -ne 0 ]]; then
+#         echo "Failed to scrape data from url: $scraper_exit_code"
+#         exit 1
+#     fi
+#     git add .
+#     git commit -m "My_bot_start executed: $timestamp"
+#     git push origin "$(git rev-parse --abbrev-ref HEAD)"
+# fi
+
+
+# Check if file exists
+echo "Executing tweepy_botV2 to scrape data, my_bot_end and data cleaner."
+bash "$scraper" "$url"
+python3 "$dir"/bot_end_runner.py
+rm -rf "$file_path"
+git add .
+git commit -m "My_bot_end executed: $timestamp"
+git push origin "$(git rev-parse --abbrev-ref HEAD)"
