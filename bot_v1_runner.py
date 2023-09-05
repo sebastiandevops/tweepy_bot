ede082ddd3f358a19db5ca3f750241f5859aa4c6
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-09-05 17:05:44 -0500

    tweet.sh, bot_v1_runner and history.txt

--- bot_v1_runner.py
+++ bot_v1_runner.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-from bots.bot_v1 import main
+
+import os
 import time
 
-maxtries = 8    # 8 * 15 minutes = about 2 hours total of waiting,
+from app.models import TweepyBot
 
-for i in range(maxtries):
-    try:
-        main()
-        break
-    except:
-        time.sleep(900)
-        print("fail", i)
+
+if __name__ == '__main__':
+
+    maxtries = 8    # 8 * 15 minutes = about 2 hours total of waiting,
+    home = os.getenv("HOME")
+    project_path = '%s/estudio/tweepy_bot' % (home)
+    data_file = '%s/scrapers/history.txt' % (project_path)
+
+    for i in range(maxtries):
+        try:
+            bot = TweepyBot(
+                hashtag="🤖 #Historia",
+                data=data_file,
+                source="[© Wikipedia]",
+                cleaner=True
+            )
+            tweet_content = bot.prepare_tweet()
+            bot.post_tweet(tweet_content)
+            print(bot.__str__())
+            break
+        except Exception as i:
+            time.sleep(900)
+            print("fail", i)
--- cronjobs/tweet.sh
+++ cronjobs/tweet.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
+
 timestamp=$(date +"%D %T")
 dir="$HOME/estudio/tweepy_bot/"
-cd "$dir" || exit
-git pull origin main
+# Change directory and handle potential errors
+cd "$dir" || {
+    echo "Failed to change directory to $dir"
+    exit 1
+}
+
+# Pull changes from the current branch and check for errors
+git pull origin "$(git rev-parse --abbrev-ref HEAD)"
+pull_exit_code=$?
+if [[ $pull_exit_code -ne 0 ]]; then
+    echo "Failed to pull changes from Git. Exit code: $pull_exit_code"
+    exit 1
+fi
+
 python3 "$dir"/bot_v1_runner.py
+
 git add .
-git commit -m "History update: $timestamp"
-git push origin main
+git commit -m "History updated: $timestamp"
+git push origin "$(git rev-parse --abbrev-ref HEAD)"
